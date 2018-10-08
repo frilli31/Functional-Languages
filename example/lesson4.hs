9d4b619b2184199c6aa60d336a2ef6c508cec874
luca <luca.all1996@gmail.com>
2018-10-09 00:10:53 +0200

    Edited example\lesson4

--- example/lesson4.hs
+++ example/lesson4.hs
@@ -9,5 +9,7 @@ myConcat xss = [x | xs <- xss, x <- xs]
 main :: IO()
 main = do
     -- List Comprehension
-    print [x^2 | x <-[1..5]]        -- x <-[1..5] is a generator
-    print (myConcat [[3,4],[37,8],[],[9]])
+    print [x^2 | x <-[1..5]]                -- x <-[1..5] is a generator
+                                            -- print: [1,4,9,16,25]
+    print (myConcat [[3,4],[37,8],[],[9]])  -- print: [3,4,37,8,9]
+    
